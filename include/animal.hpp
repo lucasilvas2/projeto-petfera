e89386bc6c177f8b46b260711b9a8298499eb5f1
Lucas <lucasoliver1068@gmail.com>
2020-11-28 18:24:59 -0300

    Ajuste nome da classe

---
+++ include/animal.hpp
@@ -0,0 +1,65 @@
+#ifndef ANIMAIS
+#define ANIMAIS
+
+#include <iomanip>
+#include "tratador.hpp"
+#include "veterinario.hpp"
+
+enum Habitat{
+	nativo,
+	exotico,
+	domestico
+};
+
+enum tipoSexo{
+	macho,
+	femea
+};
+
+class Animais{
+protected:
+	string identificacao;
+	double preco;
+	string descricao;
+	string coloracao;
+	Habitat habitat;
+	bool ameacado;
+	double peso;
+	tipoSexo sexo;
+	Tratador* tratador=nullptr;
+	Veterinario* veterinario=nullptr;
+
+public:
+	Animais(string identificacao, double preco, string descricao,
+			 string coloracao, Habitat habitat, bool ameacado, double peso, tipoSexo sexo);
+	virtual ~Animais();
+
+	string getIdentificacao() const;
+	double getPreco() const;
+	string getDescricao() const;
+	string getColoracao() const;
+	bool getAmeacado() const;
+	Habitat getHabitat() const;
+	double getPeso() const;
+	tipoSexo getSexo() const;
+	Tratador* getTratador() const;
+	Veterinario* getVeterinario() const;
+
+	void setIdentificacao(string identificacao);
+	void setPreco(double preco);
+	void setDescricao(string descricao);
+	void setColocaracao(string coloracao);
+	void setAmeacado(bool ameacado);
+	void setHabitat(Habitat habitat);
+	void setPeso(double peso);
+	void setSexo(tipoSexo sexo);
+	void setTratador(Tratador &tratador);
+	void setVeterinario(Veterinario &veterinario);
+		
+	friend ostream& operator<<(ostream& o, Animais const &a);
+private:
+	virtual ostream& print(ostream&) const = 0;
+};
+
+
+#endif
