a244b9514f19b3b1c82d01b6f25062332148d31a
Lucas <lucasoliver1068@gmail.com>
2020-11-29 20:45:53 -0300

    Teste classes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
-#include "reptil.hpp"
-#include "aves.hpp"
-#include "anfibios.hpp"
-#include "mamiferos.hpp"
+
+#include "reptil_nativo.hpp"
+#include "reptil_exotico.hpp"
+#include "reptil_domestico.hpp"
+
+#include "ave_nativa.hpp"
+#include "ave_exotica.hpp"
+#include "ave_domestica.hpp"
+
+#include "anfibio_nativo.hpp"
+#include "anfibio_domestico.hpp"
+#include "anfibio_exotico.hpp"
+
+#include "mamifero_nativo.hpp"
+#include "mamifero_exotico.hpp"
+#include "mamifero_domestico.hpp"
+
 #include "tratador.hpp"
 #include "veterinario.hpp"
-#include "funcloja.hpp"
-#include "estoque.hpp"
-#include "loja.hpp"
+
 
 int main(int argc, char const *argv[])
-{
-	/*Estoque petfera;
-	petfera.teste();*/
-
-	/*
+{	
+	
 	Tratador* lucas = new Tratador("Lucas", "84 91111-1111", "Rua replantada", vermelho);
 	Veterinario* fernando = new Veterinario("Fernando", "84 91111-1111", "Rua asfaltada", "89456-0");
 	
@@ -23,12 +31,13 @@ int main(int argc, char const *argv[])
 	cout << (*lucas) << endl;
 	cout << (*fernando) << endl;
 	
-	Reptil* largato = new Reptil("2020", 200.00, "Largato do brejo", "Rosa com pintas rosas", nativo, 1, 0.50,macho, sim, 3 );
+	ReptilNativo* largato = new ReptilNativo("2020", 200.00, "Largato do brejo", "Rosa com pintas rosas", 1, 0.50,macho, sim, 3 , "RN", "526-20");
 	largato -> setTratador(*lucas);
 	largato -> setVeterinario(*fernando);
+	cout << ((Reptil) *largato) << endl;
 	cout << (*largato) << endl;
 
-	Aves* calopsita = new Aves("3030", 100.15, "Calopsita berradeira", "Laranja", domestico, 0, 0.45, femea, carenatas, 0.89);
+	/*Aves* calopsita = new Aves("3030", 100.15, "Calopsita berradeira", "Laranja", domestico, 0, 0.45, femea, carenatas, 0.89);
 	cout << (*calopsita) << endl;
 	
 	Anfibios* sapo = new Anfibios("4040", 50.1 ,"Sapo cururu", "preto", nativo, 0, 0.30, femea , naoVenenoso);
@@ -36,37 +45,6 @@ int main(int argc, char const *argv[])
 
 	Mamiferos* cachorro = new Mamiferos("5050", 123.50 ,"Pastor Alemao", "Marrom", domestico, 0, 20.00,macho , Carnivoro);
 	cout << (*cachorro) << endl;*/
-	
-	
-	FuncLoja testefuncionarios; //criando vetor funcionarios
-	/*
-	testefuncionarios.criarFunc();
-	testefuncionarios.criarFunc();
-	testefuncionarios.criarFunc();
-	testefuncionarios.listarFunc();
-	testefuncionarios.encontrarFunc("joao");
-	testefuncionarios.removerFunc("ana");
-	testefuncionarios.listarFunc();*/
-
-	Estoque testeEstoque; //criando vetor animais
-	/*
-	testeEstoque.adicionarAnimal(largato);
-	testeEstoque.adicionarAnimal(calopsita);
-	testeEstoque.adicionarAnimal(sapo);
-	testeEstoque.adicionarAnimal(cachorro);
-	testeEstoque.listarAnimais();
-	testeEstoque.encontrarAnimal("3030");
-	testeEstoque.removerAnimal("4040");
-	testeEstoque.listarAnimais();
-	testeEstoque.criarAnimal();
-	testeEstoque.listarAnimais();*/
-
-	Loja testeLoja;
-
-	testeLoja.estoque=testeEstoque;
-	testeLoja.funcionarios=testefuncionarios;
-
-	testeLoja.menu();
 
 	return 0;
 }
