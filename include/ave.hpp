7c12f7f1ee3a3234396ec5de6f49b0236ed293d2
Lucas <lucasoliver1068@gmail.com>
2020-11-29 01:08:19 -0300

    Mudança do nome do arquivo.

---
+++ include/ave.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "animal.hpp"
+enum HabilidadeVoo{
+    ratitas, //não voam 
+    carenatas //voam
+};
+class Ave : public Animal{
+    private:
+        string classe = "Aves";
+        HabilidadeVoo habVoo;
+        double envergadura;
+
+        ostream& print(ostream& o) const;
+    public:
+        Ave(string identificacao, double preco, string descricao,string coloracao, bool ameacado, 
+                double peso, tipoSexo sexo, HabilidadeVoo habVoo, double envergadura);
+        ~Ave();
+        HabilidadeVoo getHabilidadeVoo() const;
+        double getEnvergadura() const;
+
+        void setHabilidadeVoo(HabilidadeVoo habVoo);
+        void setEnvergadura(double envergadura);
+};
