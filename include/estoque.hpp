b3ea13626e0afc33694059b49622dfa566bc8a84
Lucas <lucasoliver1068@gmail.com>
2020-12-02 19:44:36 -0300

    Arquivos de cabeçalho.

--- include/estoque.hpp
+++ include/estoque.hpp
@@ -2,6 +2,24 @@
 #define ESTOQUE
 
 #include "animal.hpp"
+#include "reptil_nativo.hpp"
+#include "reptil_exotico.hpp"
+#include "reptil_domestico.hpp"
+
+#include "ave_nativa.hpp"
+#include "ave_exotica.hpp"
+#include "ave_domestica.hpp"
+
+#include "anfibio_nativo.hpp"
+#include "anfibio_domestico.hpp"
+#include "anfibio_exotico.hpp"
+
+#include "mamifero_nativo.hpp"
+#include "mamifero_exotico.hpp"
+#include "mamifero_domestico.hpp"
+
+#include "tratador.hpp"
+#include "veterinario.hpp"
 #include "funcloja.hpp"
 #include <iostream>
 #include <vector>
@@ -10,6 +28,7 @@ using std::vector;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::getline;
 
 class Estoque{
 private:
