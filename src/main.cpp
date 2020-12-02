a6e96288b3a9d05ec0bab007168f8a028286a344
Lucas <lucasoliver1068@gmail.com>
2020-12-02 19:48:15 -0300

    Teste do programa.

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
-
-#include "reptil_nativo.hpp"
-#include "reptil_exotico.hpp"
-#include "reptil_domestico.hpp"
-
-#include "ave_nativa.hpp"
-#include "ave_exotica.hpp"
-#include "ave_domestica.hpp"
-
-#include "anfibio_nativo.hpp"
-#include "anfibio_domestico.hpp"
-#include "anfibio_exotico.hpp"
-
-#include "mamifero_nativo.hpp"
-#include "mamifero_exotico.hpp"
-#include "mamifero_domestico.hpp"
-
-#include "tratador.hpp"
-#include "veterinario.hpp"
-
-#include "petshop.hpp"
+#include "loja.hpp"
 
 
 int main(int argc, char const *argv[])
 {	
-	Petshop pet;
-	Tratador* lucas = new Tratador("Lucas", "84 91111-1111", "Rua replantada", vermelho);
+	/*Tratador* lucas = new Tratador("Lucas", "84 91111-1111", "Rua replantada", vermelho);
 	Veterinario* fernando = new Veterinario("Fernando", "84 91111-1111", "Rua asfaltada", "89456-0");
 	
 
 	cout << (*lucas) << endl;
 	cout << (*fernando) << endl;
 	
-	ReptilNativo* largato = new ReptilNativo("2020", 200.00, "Largato do brejo", "Rosa com pintas rosas", 1, 0.50,macho, sim, 3 , "RN", "526-20");
+	ReptilNativo* largato = new ReptilNativo("2020", 50, "Largato do brejo", 0.50, femea, sim, 100, "RN", 1, "056894");
 	largato -> setTratador(*lucas);
 	largato -> setVeterinario(*fernando);
 	cout << ((Reptil) *largato) << endl;
-	cout << (*largato) << endl;
+	cout << (*largato) << endl;*/
 
 	/*Aves* calopsita = new Aves("3030", 100.15, "Calopsita berradeira", "Laranja", domestico, 0, 0.45, femea, carenatas, 0.89);
 	cout << (*calopsita) << endl;
@@ -46,9 +25,15 @@ int main(int argc, char const *argv[])
 
 	Mamiferos* cachorro = new Mamiferos("5050", 123.50 ,"Pastor Alemao", "Marrom", domestico, 0, 20.00,macho , Carnivoro);
 	cout << (*cachorro) << endl;*/
+	Loja testeLoja;
 
-	pet.addAnimal(largato);
+	FuncLoja testefuncionarios;
+	Estoque testeEstoque;
+
+	testeLoja.estoque=testeEstoque;
+	testeLoja.funcionarios=testefuncionarios;
+
+	testeLoja.menu();
 	
-
 	return 0;
 }
